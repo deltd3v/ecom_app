dd1867a42d983f314e6e1ac1cf9b01eb5e79ce8d
deltd3v <deltd3v@gmail.com>
2022-07-21 02:05:29 +0300

    � works like a charm, added vector-icons

---
+++ track_way/src/screens/ModuleScreen/ModuleScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ModuleScreen = () => {
+  return (
+    <View>
+      <Text>ModuleScreen</Text>
+    </View>
+  )
+}
+
+export default ModuleScreen
+
+const styles = StyleSheet.create({})
